dfb7037f41b9ea6a7e0ba6abd6b918cf8b827b66
Zuriefais <74775691+Zuriefais@users.noreply.github.com>
2024-04-17 16:05:36 +0300

    return of quads

--- src/constants.rs
+++ src/constants.rs
@@ -1,22 +1,17 @@
 use crate::Vertex;
 
 pub const VERTICES: &[Vertex] = &[
-    // Changed
     Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-    }, // A
+        position: [-1.0, 1.0, 0.0],
+    },
     Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-    }, // B
+        position: [-1.0, -1.0, 0.0],
+    },
     Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-    }, // C
+        position: [1.0, -1.0, 0.0],
+    },
     Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-    }, // E
+        position: [1.0, 1.0, 0.0],
+    },
 ];
-
-pub const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
