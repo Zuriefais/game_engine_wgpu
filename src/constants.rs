6e87a4f77646e4887afb3d5ab9e9a86aecc8b15c
Zuriefais <74775691+Zuriefais@users.noreply.github.com>
2024-04-17 16:07:16 +0300

    quad size fix

--- src/constants.rs
+++ src/constants.rs
@@ -2,16 +2,16 @@ use crate::Vertex;
 
 pub const VERTICES: &[Vertex] = &[
     Vertex {
-        position: [-1.0, 1.0, 0.0],
+        position: [-0.5, 0.5, 0.0],
     },
     Vertex {
-        position: [-1.0, -1.0, 0.0],
+        position: [-0.5, -0.5, 0.0],
     },
     Vertex {
-        position: [1.0, -1.0, 0.0],
+        position: [0.5, -0.5, 0.0],
     },
     Vertex {
-        position: [1.0, 1.0, 0.0],
+        position: [0.5, 0.5, 0.0],
     },
 ];
 pub const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
